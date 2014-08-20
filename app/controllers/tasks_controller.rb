09bdd7deb8926e384f68b5a8f04b4b25ebbc947b
Anna Valkov & Andrew M Westling <student@epicodus.com>
2014-08-20 12:09:16 -0700

    add destroy method and route for /tasks/delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.all
+    @tasks = Task.all.where(completed:false)
     render('tasks/index.html.erb')
   end
 
@@ -10,8 +10,28 @@ class TasksController < ApplicationController
   end
 
   def create
-    @tasks = Task.all
+    @tasks = Task.all.where(completed:false)
     @task = Task.create(:name => params[:name])
     render('tasks/index.html.erb')
   end
+
+  def complete_task
+    @task = Task.find_by(:id => params[:id])
+    @tasks = Task.all.where(completed:false)
+    @task.update(completed: true)
+    render('tasks/index.html.erb')
+  end
+
+  def completed
+    @tasks = Task.all.where(completed:true)
+    render('tasks/completed.html.erb')
+  end
+
+  def delete_task
+    @task = Task.find_by(:id => params[:id])
+    @task.destroy
+    @tasks = Task.all.where(completed:true)
+    render('tasks/completed.html.erb')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   match('tasks', {:via => :get, :to => 'tasks#index'})
   match('tasks', {:via =>:post, :to => 'tasks#create'})
+  match('tasks/:id', {:via => [:patch, :put], :to => 'tasks#complete_task'})
   match('tasks/new', {:via => :get, :to => 'tasks#new'})
+  match('tasks/completed', {:via => :get, :to => 'tasks#completed'})
+  match('tasks/delete/:id', {:via => [:patch, :put], :to => 'tasks#delete_task'})
 end
